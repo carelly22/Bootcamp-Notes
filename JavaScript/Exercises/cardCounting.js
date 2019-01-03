a312af86e71587f4de6a4e6c39f8369ab2fa3f84
Carell Ayanna Hamil <ayannahamil@gmail.com>
2019-01-03 11:27:22 -0500

    add js exercises

--- JavaScript/Exercises/cardCounting.js
+++ JavaScript/Exercises/cardCounting.js
@@ -1,7 +1,13 @@
 /*
 Blackjack counting cards problem
 
-You will write a card counting function. It will receive a card parameter, which can be a number or a string, and increment or decrement the global count variable according to the card's value (see table). The function will then return a string with the current count and the string Bet if the count is positive, or Hold if the count is zero or negative. The current count and the player's decision (Bet or Hold) should be separated by a single space.
+You will write a card counting function.It will receive a card parameter, which 
+can be a number or a string, and increment or decrement the global count variable 
+according to the card's value (see table). The function will then return a string 
+with the current count and the string Bet if the count is positive, or Hold if the 
+count is zero or negative. The current count and the player's decision (Bet or Hold) 
+should be separated by a single space.
+
 */
 
 //initialize count variable
