83036b4bf2f0feba614b2457727298245706e03b
carell hamil <ayannahamil@gmail.com>
2019-03-16 08:16:51 -0400

    add notes

--- Intro-to-Programming/consonant_cancel.rb
+++ Intro-to-Programming/consonant_cancel.rb
@@ -2,8 +2,33 @@
 # where every word begins with it's first vowel
 
 def consonant_cancel(sentence)
-
+	vowels = "aeiou"
+	main_arr = sentence.split
+	arr = main_arr.map do |word|
+		word_iterate(word)
+	end
+	return arr.join(" ")
 end
 
-puts consonant_cancel("down the rabbit hole") #=> "own e abbit ole"
-puts consonant_cancel("writing code is challenging") #=> "iting ode is allenging"
+def word_iterate(ele)
+	vowels = "aeiou"
+	i = 0
+	new_word = ""
+	while i < ele.length
+		if (vowels.include?(ele[i]))
+			return new_word = ele[i..-1]
+		end
+		i += 1
+	end
+end
+
+# print word_iterate("challenging")
+
+print consonant_cancel("down the rabbit hole") #=> "own e abbit ole"
+puts
+print consonant_cancel("writing code is challenging") #=> "iting ode is allenging"
+
+# need to split string into array
+# split each word into chars?
+# iterate through chars
+# unshift non-vowels
--- Intro-to-Programming/same_char_collapse.rb
+++ Intro-to-Programming/same_char_collapse.rb
@@ -8,12 +8,26 @@
 # "zzzxaaxy" > "zxaaxy" > "zxxy" > "zy"
 
 def same_char_collapse(str)
+	collapsible = true
 
+	while collapsible
+		collapsible = false
+
+		chars = str.split("")
+		chars.each.with_index do |char, i|
+			if chars[i] == chars[i+1]
+				chars[i] = ""
+				chars[i+1] = ""
+				collapsible = true
+				break
+			end
+		end
+		str = chars.join("")
+	end
+	return str
 end
 
 puts same_char_collapse("zzzxaaxy")   #=> "zy"
 # because zzzxaaxy -> zxaaxy -> zxxy -> zy
-
-
-puts same_char_collapse("uqrssrqvtt") #=> "uv"
+puts same_char_collapse("uqrssrqvtt") #="uv"
 # because uqrssrqvtt -> uqrrqvtt -> uqqvtt -> uvtt -> uv
