81e40843edbb62cb371c3177a930cf5eafe697b0
carellayanna <ayannahamil@gmail.com>
2019-04-04 17:56:04 -0400

    add recap exercises lectures

---
+++ Software Engineering Foundations/Recap/recap_exercise_2_lecture/most_frequent_bigram.rb
@@ -0,0 +1,18 @@
+# Write a method, most_frequent_bigram, that takes in a string and returns the two adjacent letters that appear the
+# most in the string.
+
+# use a counter hash
+
+def most_frequent_bigram(str)
+    counts = Hash.new(0)
+    (0...str.length).each do |i|
+        bigram = str[i..i+1]
+        counts[bigram] += 1
+    end
+
+    # sort_by is a method to sort the hash
+    # returns a sorted array of keys and values
+
+    sorted = counts.sort_by { |k,v| v} 
+    sorted.last[0] 
+end 
